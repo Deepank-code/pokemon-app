c1b2881f7a4c273dbf94d126e1c5969e5ab40fe7
deepank joshi <deepsdj37@gmail.com>
2023-08-22 16:34:46 +0530

    refactoring our code by handling advance use state

--- src/Components/pokemons/Pokemon.jsx
+++ src/Components/pokemons/Pokemon.jsx
@@ -4,21 +4,33 @@ import Loader from "../Loader.jsx";
 import "./Pokemon.css";
 import PokemonCard from "../Pokemon card/PokemonCard.jsx";
 function Pokemon() {
-  const [pokemonList, setPokemonList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [pokemonUrl, setPokemonUrl] = useState(
-    "https://pokeapi.co/api/v2/pokemon/"
-  );
-  const [nextPokemon, setNextPokemon] = useState("");
-  const [prevPokemon, setPrevPokemon] = useState("");
+  const [pokeListState, setPokeListState] = useState({
+    pokemonList: [],
+    isLoading: true,
+    pokemonUrl: "https://pokeapi.co/api/v2/pokemon/",
+    nextPokemonUrl: "",
+    prevPokemonUrl: "",
+  });
   const fetchPokemon = async () => {
-    setIsLoading(true);
-    const data = await fetch(pokemonUrl);
+    setPokeListState((prevState) => {
+      return {
+        ...prevState,
+        isLoading: true,
+      };
+    });
+    const data = await fetch(pokeListState.pokemonUrl);
 
     const result = await data.json();
     console.log(result);
-    setNextPokemon(result.next);
-    setPrevPokemon(result.previous);
+
+    setPokeListState((prevState) => {
+      return {
+        ...prevState,
+        nextPokemonUrl: result.next,
+        prevPokemonUrl: result.previous,
+      };
+    });
+
     const pokeurl = result.results;
 
     const allPokemon_url = pokeurl.map((poke) => {
@@ -33,38 +45,42 @@ function Pokemon() {
     });
     const pokemon_data = await Promise.all(poke_data);
 
-    setPokemonList(
-      pokemon_data.map((obj) => {
-        return {
-          id: obj.id,
-          name: obj.name,
-          weight: obj.weight,
-          image: obj.sprites.other
-            ? obj.sprites.other.dream_world.front_default
-            : obj.sprites.other.front_shiny,
-          // type: obj.types.type,
-        };
-      })
-    );
-
-    setIsLoading(false);
+    console.log(pokemon_data);
+    setPokeListState((prevState) => {
+      return {
+        ...prevState,
+        pokemonList: pokemon_data.map((obj) => {
+          return {
+            id: obj.id,
+            name: obj.name,
+            weight: obj.weight,
+            image: obj.sprites.other
+              ? obj.sprites.other.dream_world.front_default
+              : obj.sprites.other.front_shiny,
+            // type: obj.types.type,
+          };
+        }),
+        isLoading: false,
+      };
+    });
   };
-
+  console.log(pokeListState);
   useEffect(() => {
     fetchPokemon();
-  }, [pokemonUrl]);
+  }, [pokeListState.pokemonUrl]);
+
   return (
     <>
       <div className="pokemon-wrapper">
         <h1 className="text-center my-4">Pokemon</h1>
         <Search />
         <div className="text-center my-5">
-          {isLoading ? (
+          {pokeListState.isLoading ? (
             <Loader />
           ) : (
             <>
               <div className="poke-desk">
-                {pokemonList.map((poke) => {
+                {pokeListState.pokemonList.map((poke) => {
                   return (
                     <PokemonCard
                       key={poke.id}
@@ -78,19 +94,29 @@ function Pokemon() {
               </div>
               <div>
                 <button
-                  disabled={prevPokemon === null ? true : false}
+                  disabled={
+                    pokeListState.prevPokemonUrl === null ? true : false
+                  }
                   className="btn btn-outline-danger prev m-5"
                   onClick={() => {
-                    setPokemonUrl(nextPokemon);
+                    setPokeListState({
+                      ...pokeListState,
+                      pokemonUrl: pokeListState.prevPokemonUrl,
+                    });
                   }}
                 >
                   Pre
                 </button>
                 <button
-                  disabled={nextPokemon === null ? true : false}
+                  disabled={
+                    pokeListState.nextPokemonUrl === null ? true : false
+                  }
                   className="btn btn-outline-success next m-5"
                   onClick={() => {
-                    setPokemonUrl(nextPokemon);
+                    setPokeListState({
+                      ...pokeListState,
+                      pokemonUrl: pokeListState.nextPokemonUrl,
+                    });
                   }}
                 >
                   Next
